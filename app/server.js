637d548c9c2c82fdbc1729b4fd71daae20e97b13
Mao Ye <cnkmym@gmail.com>
2018-01-10 10:01:02 +0900

    refs # update code to differenciate ips

--- app/server.js
+++ app/server.js
@@ -1,59 +1,72 @@
-'use strict'
+(function() {
+  'use strict';
 
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const errorhandler = require('errorhandler');
-const ip = require('ip');
-const version = require('../version.json');
+  const express = require('express');
+  const logger = require('morgan');
+  const bodyParser = require('body-parser');
+  const errorhandler = require('errorhandler');
+  const ip = require('ip');
+  const version = require('../version.json');
 
-const app = express();
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended':false}));
-app.use(errorhandler());
+  const app = express();
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    'extended': false
+  }));
+  app.use(errorhandler());
 
-app.get('/api/whoami',(req,res)=>{
-  const ipAddress = ip.address();
-  res.status(200).send(`I am server instance on ${ipAddress}`);
-});
+  app.get('/api/whoami', (req, res) => {
+    const ipAddress = ip.address();
+    res.status(200).send(`I am server instance on ${ipAddress}`);
+  });
 
-app.get('/api/crashme',(req,res)=>{
-  const ipAddress = ip.address();
-  console.log(`Ready to crash server instance on ${ipAddress}`);
-  process.exit(0);
-});
+  app.get('/api/crashme', (req, res) => {
+    const ipAddress = ip.address();
+    console.log(`Ready to crash server instance on ${ipAddress}`);
+    process.exit(0);
+  });
 
-app.get('/',(req,res)=>{
-  let hello = 'Hello, You are from IP_ADDRESS';
-  let newHello = hello.replace(/IP_ADDRESS/g,req.headers['x-forwarded-for'] || req.connection.remoteAddress);
-  res.status(200).send(newHello);
-});
-
-if (process.env.VCAP_SERVICES){
-  console.log(JSON.parse(process.env.VCAP_SERVICES));
-}
-
-const port = process.env.PORT || 3000;
-let server = app.listen(port,(error)=>{
-  if (error){
-    console.error(error.message);
-    process.exit(1);
-  }else{
-    console.log(`Server is running and listening on port ${port}`);
-    console.log(`Current Code version is ${version.branch}`);
-  }
-});
-
-module.exports = {
-  closeServer : ()=>{
-    if (server){
-      server.close(()=>{
-        console.log("Server is shutdown");
-      });
-      return true;
+  app.get('/', (req, res) => {
+    let ips = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    let REMOTE_IP = "N/A";
+    let BLUEMIX_LB_IP = "N/A";
+    if ((ips instanceof Array)){
+      //in bluemix environment
+      REMOTE_IP = ips[0];
+      BLUEMIX_LB_IP = ips[1];
     }else{
-      return false;
+      //local mode
+      REMOTE_IP = ips;
     }
+    res.status(200).send(`Hello, You are from <Remote IP>${REMOTE_IP}  <Bluemix LB IP>${BLUEMIX_LB_IP}`);
+  });
+
+  if (process.env.VCAP_SERVICES) {
+    console.log(JSON.parse(process.env.VCAP_SERVICES));
   }
-}
+
+  const port = process.env.PORT || 3000;
+  let server = app.listen(port, (error) => {
+    if (error) {
+      console.error(error.message);
+      process.exit(1);
+    } else {
+      console.log(`Server is running and listening on port ${port}`);
+      console.log(`Current Code version is ${version.branch}`);
+    }
+  });
+
+  module.exports = {
+    closeServer: () => {
+      if (server) {
+        server.close(() => {
+          console.log("Server is shutdown");
+        });
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+}());
--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -1,38 +1,40 @@
-'use strict'
+(function() {
+  'use strict';
 
-const request = require('request');
-const app = require('../app/server.js');
+  const request = require('request');
+  const app = require('../app/server.js');
 
-const base_url = "http://localhost:"+(process.env.PORT||3000);
+  const base_url = "http://localhost:" + (process.env.PORT || 3000);
 
-describe("Server Route Test",()=>{
+  describe("Server Route Test", () => {
 
-  describe("GET /", ()=>{
-      it("returns status code 200",(done)=>{
-        request.get(base_url,(error,response,body)=>{
+    describe("GET /", () => {
+      it("returns status code 200", (done) => {
+        request.get(base_url, (error, response, body) => {
           expect(response.statusCode).toBe(200);
           done();
         });
       });
-      it("contains valid html",(done)=>{
-        request.get(base_url,(error,response,body)=>{
+      it("contains valid html", (done) => {
+        request.get(base_url, (error, response, body) => {
           expect(body).toContain("Hello, You are from");
           done();
         });
       });
-  });
+    });
 
-  describe("GET /api/*", ()=>{
-    let path = "/api/whoami";
-    it("listens on api calls",(done)=>{
-      request.get(base_url+path,(error,response,body)=>{
-        expect(body).toContain("I am server instance on");
-        done();
+    describe("GET /api/*", () => {
+      let path = "/api/whoami";
+      it("listens on api calls", (done) => {
+        request.get(base_url + path, (error, response, body) => {
+          expect(body).toContain("I am server instance on");
+          done();
+        });
       });
     });
-  });
 
-  afterAll(function(){
-    expect(app.closeServer()).toBe(true);
+    afterAll(function() {
+      expect(app.closeServer()).toBe(true);
+    });
   });
-});
+}());
