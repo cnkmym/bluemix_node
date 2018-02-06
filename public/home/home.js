e75adec0a73f1cc23c1d8a9939e5f97b044a9f50
Mao Ye <cnkmym@gmail.com>
2018-02-06 23:28:38 +0900

    refs # update index.html

--- public/home/home.js
+++ public/home/home.js
@@ -1,4 +1,5 @@
 $("a.whoami").click(function(){
+    $("ad.whoami").html('<span style="color:grey">Loading Server Instance IP ...</span>');
     $.ajax({url: "/api/whoami", success: function(result){
       var dt = new Date();
       var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
@@ -8,25 +9,28 @@ $("a.whoami").click(function(){
 
 $("a.sleep-10").click(function(){
     $(this).addClass("disabled btn-default").removeClass("btn-success");
+    $("td.sleep-10").html('<span style="color:grey">Timer Start</span>');
     $.ajax({url: "/api/sleep/10", success: function(result){
       $("a.sleep-10").removeClass("disabled btn-default").addClass("btn-success");
       var dt = new Date();
       var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-      $("td.sleep-10").html(result + "   Finished @ "+time);
+      $("td.sleep-10").html(result + "   ,Finished @ "+time);
     }});
 });
 
-$("a.slepp-100").click(function(){
+$("a.sleep-300").click(function(){
     $(this).addClass("disabled btn-default").removeClass("btn-warning");
+    $("td.sleep-300 ").html('<span style="color:grey">Timer Start</span>');
     $.ajax({url: "/api/sleep/100", success: function(result){
-      $("a.sleep-100").removeClass("disabled btn-default").addClass("btn-warning");
+      $("a.sleep-300").removeClass("disabled btn-default").addClass("btn-warning");
       var dt = new Date();
       var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-      $("td.slepp-100").html(result + "   Finished @ "+time);
+      $("td.sleep-300").html(result + "   ,Finished @ "+time);
     }});
 });
 
 $("a.crashme").click(function(){
+    $("td.crashme").html('<span style="color:grey">Force Shutdown</span>');
     $.ajax({url: "/api/crashme", error: function(result){
       var dt = new Date();
       var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
